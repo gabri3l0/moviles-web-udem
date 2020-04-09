b365809e80dfb7f34eb33aa471f26f7ac4e699a3
gabri3l0 <42877617+gabri3l0@users.noreply.github.com>
2020-04-08 22:57:20 -0500

    Add files

---
+++ firebase.js
@@ -0,0 +1,14 @@
+(function() {
+  const config = {
+    apiKey: "AIzaSyBaN8PbRty3hNbP9uffPUrRoaC6ZvUAgZ4",
+    authDomain: "moviles-udem.firebaseapp.com",
+    databaseURL: "https://moviles-udem.firebaseio.com",
+    projectId: "moviles-udem",
+    storageBucket: "moviles-udem.appspot.com",
+    messagingSenderId: "1068989790164",
+    appId: "1:1068989790164:web:a860f5172ab0e54cad0841"
+    };
+
+  firebase.initializeApp(config);
+
+}());
